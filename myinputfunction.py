6a81d93e99a60a5ef7e8f1f631117f8fa2a7e0d2
BeleSakshi <belesakshi999@gmail.com>
2024-09-27 10:56:54 +0530

    Initial commit

---
+++ myinputfunction.py
@@ -0,0 +1,7 @@
+# The following will ask the user for a name
+name = input ("What is your name?: ")
+print ("Nice to meet you " + name + "!")
+age = input ("Your age? ")
+print ("So, you are already " + age + " years old, " + name + "!")
+color = input ("What is your fav color?: ")
+print ("So, you fav color is " + color + "!")
